bfe23e4bc221db3d8b53e11f415945cdfdc2d760
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2024-05-07 13:48:21 +0200

    Ajuste de textarea

--- src/app/prompt/prompt.module.ts
+++ src/app/prompt/prompt.module.ts
@@ -21,7 +21,6 @@ import { TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
 import { PromptViewComponent } from './views/prompt-view/prompt-view.component';
 import { BadgeModule } from 'primeng/badge';
-import {AutosizeModule} from 'ngx-autosize';
 
 @NgModule({
   declarations: [
@@ -51,7 +50,6 @@ import {AutosizeModule} from 'ngx-autosize';
     AutoCompleteModule,
     ToastModule,
     BadgeModule,
-    AutosizeModule,
   ]
 })
 export class PromptModule { }
