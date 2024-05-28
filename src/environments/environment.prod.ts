7465475dab66d042cd8cf6ac30375ac3de7880ad
Pablo Jimenez <pablo.jimenez-martinez@capgemini.com>
2024-05-28 17:20:46 +0200

    cambio de dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   appCode: 'promptlibrary',
-  server: '../promptpedia-data',
+  server: '../promptlibrary-data',
   sso: '../sso',
   ssoApp: '/apps'
 };
